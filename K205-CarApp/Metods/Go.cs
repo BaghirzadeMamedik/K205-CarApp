7730a30d8874e68020ea662a1773d0cec88e074b
BaghirzadeMamedik <brizeforge@gmail.com>
2022-01-14 16:19:38 +0400

    Add project files.

---
+++ K205-CarApp/Metods/Go.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K205_CarApp.Metods
+{
+    public class Go
+    {
+        public void HereketeBasla(decimal benzin)
+        {
+            Console.WriteLine("Nece Km yol geceksiniz?");
+
+            string yol = Console.ReadLine();
+
+            decimal netice = (Convert.ToDecimal(yol)* 10) / 100;
+
+            decimal qalanBenzin = benzin - netice;
+
+            Console.WriteLine("Sizin " +  qalanBenzin + " litr benzininiz qaldi");
+            
+        }
+    }
+}
---
+++ K205-CarApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using K205_CarApp.Metods;
+
+Console.WriteLine("1. Yola cix");
+Console.WriteLine("2. Benzin doldur");
+Console.WriteLine("3. Yanacaq miqdari");
+Console.WriteLine("4. Yurush");
+Console.WriteLine("5. Cixish");
+//////////////////////////////////////////
+
+string daxilEdilenReqem = Console.ReadLine();
+
+Go start = new Go();
+
+decimal benzin = 100M;
+
+/// //////////////////////////////////////
+
+
+switch (Convert.ToInt32(daxilEdilenReqem))
+{
+    case 1:
+        start.HereketeBasla(benzin);
+
+        break;
+    case 2:
+
+
+        break;
+    case 3:
+
+
+        break;
+    case 4:
+
+
+        break;
+    case 5:
+
+
+        break;
+    default:
+
+        break;
+}
+
